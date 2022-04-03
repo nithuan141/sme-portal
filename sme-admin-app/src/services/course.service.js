992d1166c1afb90be13faaed218236abd11bfb81
Aravindsreeni <aravindsreeni@gmail.com>
2022-04-03 16:35:56 +0530

    Course Edit

--- sme-admin-app/src/services/course.service.js
+++ sme-admin-app/src/services/course.service.js
@@ -1,13 +1,17 @@
-import httpHelper from './httpserviceutils/HttpHelper'
+import httpHelper from "./httpserviceutils/HttpHelper";
 
 export const getAllCourse = () => {
-    return httpHelper.get(`v1/Course`)
-}
+  return httpHelper.get(`v1/Course`);
+};
 
 export const saveCourse = (course) => {
-    return httpHelper.post('v1/Course', course)
-}
+  return httpHelper.post("v1/Course", course);
+};
+
+export const updateCourse = (course) => {
+  return httpHelper.put("v1/Course", course);
+};
 
 export const uploadFile = (formData) => {
-    return httpHelper.post('v1/Course/upload', formData);
-}
+  return httpHelper.post("v1/Course/upload", formData);
+};
