2d63774c8c6fa37d2d7bbcf9e08f9ac53d8ab3f9
Nithesh Narayanan <nithuan141@gmail.com>
2022-05-15 23:34:34 +0530

    fix

--- sme-admin-app/src/pages/LoginPage.jsx
+++ sme-admin-app/src/pages/LoginPage.jsx
@@ -27,7 +27,7 @@ const LoginPage = (props) => {
 
     const login = () => {
         authenticate(user).then(response => {
-            if( response.status === 200) {
+            if( response.status === 200 && response?.data.role == "Admin") {
                 setLoginError(false)
                 setLoggedInUser(response.data)
                 history.push('/home')
